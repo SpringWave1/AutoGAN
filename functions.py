2af979808fc452f4ca2de442872bf84fc9d1b8ef
juntaozhao <juntaozhao@tencent.com>
2021-03-16 22:50:02 +0800

    modify epoch

--- functions.py
+++ functions.py
@@ -157,7 +157,8 @@ def train(args, gen_net: nn.Module, dis_net: nn.Module, gen_optimizer, dis_optim
             gen_optimizer.zero_grad()
 
             gen_z = torch.cuda.FloatTensor(np.random.normal(0, 1, (args.gen_batch_size, args.latent_dim)))
-            gen_imgs = gen_net(gen_z, epoch)
+            # gen_imgs = gen_net(gen_z, epoch)
+            gen_imgs = gen_net(gen_z)
             fake_validity = dis_net(gen_imgs)
 
             # cal loss
