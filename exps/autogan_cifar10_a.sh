de12b05ed7b2cedef29a46226b28d95196e1e8f8
juntaozhao <juntaozhao@tencent.com>
2021-03-16 23:19:14 +0800

    change cifar_a to 500

--- exps/autogan_cifar10_a.sh
+++ exps/autogan_cifar10_a.sh
@@ -6,7 +6,7 @@ python train.py \
 --dataset cifar10 \
 --bottom_width 4 \
 --img_size 32 \
---max_iter 50000 \
+--max_iter 500 \
 --gen_model autogan_cifar10_a \
 --dis_model autogan_cifar10_a \
 --latent_dim 128 \
